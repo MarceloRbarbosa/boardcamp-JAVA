e104a17f3266ff76429d75262c11533b6bd1222d
Marcelo R. Barbosa <marceloribeirobarbosa92@gmail.com>
2025-12-05 11:11:24 -0300

    feat: Rental Unit Test

--- src/test/java/com/boardcamp/api/Unit/RentalsUnitTests.java
+++ src/test/java/com/boardcamp/api/Unit/RentalsUnitTests.java
@@ -22,59 +22,59 @@ import com.boardcamp.api.services.RentalsService;
 @ExtendWith(MockitoExtension.class)
 public class RentalsUnitTests {
 
-    @InjectMocks
-    private RentalsService rentalsService;
+        @InjectMocks
+        private RentalsService rentalsService;
 
-    @Mock
-    private RentalsRepository rentalsRepository;
+        @Mock
+        private RentalsRepository rentalsRepository;
 
-    @Mock
-    private CustomersService customersService;
+        @Mock
+        private CustomersService customersService;
 
-    @Mock
-    private CustomerRepository customerRepository;
+        @Mock
+        private CustomerRepository customerRepository;
 
-    @Mock
-    GamesService gamesService;
+        @Mock
+        GamesService gamesService;
 
-    @Test
-    void givenDaysRentedZero_whenPostRental_thenThrowsNegativeValueException() {
+        @Test
+        void givenDaysRentedZero_whenPostRental_thenThrowsNegativeValueException() {
 
-        rentalsDTO dto = new rentalsDTO(
-                1L,
-                1L,
-                0);
+                rentalsDTO dto = new rentalsDTO(
+                                1L,
+                                1L,
+                                0);
 
-        NegativeValueException ex = assertThrows(
-                NegativeValueException.class,
-                () -> rentalsService.postRental(dto));
+                NegativeValueException ex = assertThrows(
+                                NegativeValueException.class,
+                                () -> rentalsService.postRental(dto));
 
-        assertEquals(
-                "Days rented must be have a positive value",
-                ex.getMessage());
-    }
+                assertEquals(
+                                "Days rented must be have a positive value",
+                                ex.getMessage());
+        }
 
-    @Test
-    void givenNullFields_whenPostRental_thenThrowEmptyFieldException() {
-        rentalsDTO dto = new rentalsDTO(null,
-                null,
-                5);
+        @Test
+        void givenNullFields_whenPostRental_thenThrowEmptyFieldException() {
+                rentalsDTO dto = new rentalsDTO(null,
+                                null,
+                                5);
 
-        EmptyFieldException ex = assertThrows(
-                EmptyFieldException.class,
-                () -> rentalsService.postRental(dto));
+                EmptyFieldException ex = assertThrows(
+                                EmptyFieldException.class,
+                                () -> rentalsService.postRental(dto));
 
-        assertEquals("Customer and gameId not be null",
-                ex.getMessage());
-    }
+                assertEquals("Customer and gameId not be null",
+                                ex.getMessage());
+        }
 
-    @Test
-    void givenInvalidGameId_whenPostRental_thenThrowsInvalidGameIdException() {
+        @Test
+        void givenInvalidGameId_whenPostRental_thenThrowsInvalidGameIdException() {
 
-        rentalsDTO dto = new rentalsDTO(1L, 0L, 3);
+                rentalsDTO dto = new rentalsDTO(1L, 0L, 3);
 
-        assertThrows(
-                InvalidGameIdException.class,
-                () -> rentalsService.postRental(dto));
-    }
+                assertThrows(
+                                InvalidGameIdException.class,
+                                () -> rentalsService.postRental(dto));
+        }
 }
